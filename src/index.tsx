9655f9d139c95e2abf34847f8c97150b11b8e1f8
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-21 17:17:12 +0900

    ☔ Modify : webpack 소스 변경
    1. plugin설정(uglifyjsplugin, cleanWebpackplugin)
    2. font loader 설정(file-loader)
    3. devserver 설정(port 8000)
    4. tsconfig와 webpack.config.js의 alias 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import EditableMathExample from "./EditableMathExample";
+import EditableMathExample from "@src/EditableMathExample";
 
 ReactDOM.render(<EditableMathExample/>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const Dotenv = require('dotenv-webpack');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const webpack = require('webpack');
 module.exports = {
     entry:"./src/index",
     output:{
@@ -10,6 +12,9 @@ module.exports = {
     mode:"none",
     resolve:{
         extensions:['.tsx', '.ts','.js','.jsx'],
+        alias:{
+            "@src" : path.resolve(__dirname, 'src')
+        }
     },
     module:{
         rules:[
@@ -23,9 +28,14 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(js|jsx)$/,
-                exclude: "/node_modules",
+                test: /\.jsx?$/,
                 use: ['babel-loader'],
+                exclude: "/node_modules",
+            },
+            // font loader 
+            {
+                test:/\.(woff|woff2|eot|ttf|otf)$/,
+                loader:'flie-loader',
             },
             {
                 test:/\.tsx?$/,
@@ -34,11 +44,21 @@ module.exports = {
             },
         ],
     },
+    devServer:{
+        contentBase: path.resolve(__dirname,'build'),
+        port:8000,
+    },
     plugins: [
         new HtmlWebPackPlugin({
           template: './public/index.html', // public/index.html 파일을 읽는다.
           filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
         }),
-        new Dotenv()
-    ]
+        new UglifyJsPlugin(),  // 로더로 처리된 자바스크립트 결과물을 난독화하는 프로그램
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['build']
+        }),
+        new webpack.BannerPlugin({
+            banner: `build time : ${new Date().toLocaleTimeString()}`
+        })
+     ] 
 }
