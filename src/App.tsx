0da1575843355335d3516e59c741c85b876df08c
zlrlo <easy10612@gmail.com>
2020-11-25 00:03:15 +0900

    test: font size 드롭다운 테스트 임시 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { addStyles, EditableMathField, StaticMathField } from 'react-mathquill';
 import Header from '@src/components/header';
+import FontDropDown from '@src/components/toolbar/FontDropDown';
 
 addStyles();
 
@@ -8,6 +9,7 @@ const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <FontDropDown />
     </div>
   );
 };
