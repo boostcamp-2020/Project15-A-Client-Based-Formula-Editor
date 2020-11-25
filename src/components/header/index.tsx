15fb774c5c2eedaa89e04f408b1b862043b557d9
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-25 16:43:05 +0900

    style : 헤더 레이아웃 설정 및 스타일 추가
    1. 헤더 레이아웃 설정 및 스타일을 추가하였습니다.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import SVGIcon from '@src/utils/svg/logo.svg';
 import Button from '@src/components/common/Button';
 import colors from '@src/utils/colors';
+import { HeaderContainer, ButtonContainer } from './style';
 
 const Header = () => {
   return (
-    <>
+    <HeaderContainer>
       <SVGIcon />
-      <Button color={colors.imageSave} value="이미지 저장" />
-      <Button color={colors.exportSave} value="export" />
-    </>
+      <ButtonContainer>
+        <Button color={colors.imageSave} value="이미지 저장" />
+        <Button color={colors.exportSave} value="export" />
+      </ButtonContainer>
+    </HeaderContainer>
   );
 };
 
