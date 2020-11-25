25724bc5dff49784724c903b2c0a5212edbd9e69
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-25 19:25:25 +0900

    feature : redux 세팅
    1. index에 provider를 작성하였습니다.
    2. toolbar를 수정하였습니다.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@src/App';
+import store from '@src/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store as any}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
