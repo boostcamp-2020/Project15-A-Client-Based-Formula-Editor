2fa22141455a36cb061518b56851c8aaef160229
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-25 16:41:34 +0900

    modify : color 16진수 수정
    1. color 16진수를 수정하였습니다.

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -4,12 +4,14 @@ interface colorType {
   fontGreen: string;
   black: string;
   borderGrey: string;
+  containerBackground: string;
 }
 const colors: colorType = {
-  imageSave: '#fbfbfb',
+  imageSave: '#D0D0D0',
   exportSave: '#B4F6E0',
   fontGreen: '#00c73c',
   black: '#333',
   borderGrey: '#e0e0e0',
+  containerBackground: '#fff',
 };
 export default colors;
