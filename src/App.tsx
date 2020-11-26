4f591d1e6c6da726c6a44a64c222a2e7a62d4dd8
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-26 12:19:45 +0900

    refactor : dropdown 컨테이너 스타일 분리
    1. dropdown 컨테이너의 스타일을 분리하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
       <Header />
       <Toolbar />
       <FontDropDown />
-      <FontColorDropdown/>
+      <FontColorDropdown />
     </div>
   );
 };
---
+++ src/components/common/dropdown/style.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const DropdownDiv = styled.div`
+  position: absolute;
+  width: 40%;
+`;
+export const DropdownContent = styled.div`
+  width: 100%;
+  height: 50px;
+  border: 1px solid;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+`;
