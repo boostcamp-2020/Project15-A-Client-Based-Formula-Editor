48ffc25cd854570d65befdbdfe40fee5e2146e21
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-26 20:06:04 +0900

    feature : 초기화 버튼 생성
    1. 초기화 버튼을 생성하였습니다.

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -3,6 +3,7 @@ import colors from '@src/utils/colors';
 import DECLINEICON from '@src/utils/svg/decline-text.svg';
 import DRAWER from '@src/utils/svg/drawer.svg';
 import BACKGROUND from '@src/utils/svg/background/background_icon.svg';
+import CLEAR from '@src/utils/svg/clear.svg';
 import ToolBarButton from '@src/components/common/toolbarbutton';
 import { useDispatch, useSelector } from 'react-redux';
 import { rootState } from '@src/store/modules';
@@ -28,33 +29,36 @@ const ToolBar = () => {
     dispatch(dropdown(!isDropdownShow));
   };
   const onClickDeclineHandler = () => {
-    console.log('decline click');
+    console.log('decline');
   };
   const onClickBackgroundHandler = () => {
     dispatch(backgroundDropdown(!isBackgroundDropdownShow));
-    console.log(isBackgroundDropdownShow);
   };
+  const onClickClearHandler = () => {
+    console.log('clear');
+  };
+  const buttonAttributes = [
+    { Svg: DECLINEICON, onClick: onClickDeclineHandler },
+    { Svg: DRAWER, onClick: onClickDrawerHandler },
+    { Svg: BACKGROUND, onClick: onClickBackgroundHandler },
+    { Svg: CLEAR, onClick: onClickClearHandler },
+  ];
   return (
     <StyledComponent.ToolbarContainer>
       <FontDropDown />
       <FontColorDropdown />
-      <ToolBarButton
-        color={colors.mainGreen}
-        Svg={DECLINEICON}
-        onClick={onClickDeclineHandler}
-      />
+      {buttonAttributes.map((value) => {
+        return (
+          <ToolBarButton
+            key={`button-${value}`}
+            color={colors.mainGreen}
+            Svg={value.Svg}
+            onClick={value.onClick}
+          />
+        );
+      })}
       {isDropdownShow && <DropDown click="drawer" />}
       {isBackgroundDropdownShow && <DropDown click="background" />}
-      <ToolBarButton
-        color={colors.mainGreen}
-        Svg={DRAWER}
-        onClick={onClickDrawerHandler}
-      />
-      <ToolBarButton
-        color={colors.mainGreen}
-        Svg={BACKGROUND}
-        onClick={onClickBackgroundHandler}
-      />
     </StyledComponent.ToolbarContainer>
   );
 };
