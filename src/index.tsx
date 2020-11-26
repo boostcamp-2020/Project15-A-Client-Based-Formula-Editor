f97c4a2a99460d7b38556b393595bd4aa9503700
zlrlo <easy10612@gmail.com>
2020-11-26 15:44:00 +0900

    fix: Provider 중복코드 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from '@src/App';
 import store from '@src/store';
-import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store as any}>
