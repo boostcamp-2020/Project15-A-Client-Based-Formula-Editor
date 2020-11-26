82a34004c7958721e001b42f8f9f3079ab31326e
zlrlo <easy10612@gmail.com>
2020-11-26 15:41:47 +0900

    feat: 탭 container 구현
    - TabList 만들고 마지막에 '+'버튼 추가

---
+++ src/components/tab/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@src/store/modules';
+import { changeTab, addTab } from '@src/store/modules/tab';
+import TabList from './TabList';
+import * as StyleComponent from './style';
+
+export default function Tab() {
+  const { selectedTabId, tabList } = useSelector(
+    (state: RootState) => state.tabReducer
+  );
+
+  const dispatch = useDispatch();
+
+  const handleChangeTab = (tabId: number) => {
+    dispatch(changeTab(tabId));
+  };
+
+  const handleAddTab = () => {
+    dispatch(addTab());
+  };
+
+  const list: JSX.Element[] = tabList.map(
+    (tab): JSX.Element => {
+      return (
+        <TabList
+          key={tab.id}
+          tabId={tab.id}
+          selectedTabId={selectedTabId}
+          tabTitle={tab.title}
+          handleChangeTab={handleChangeTab}
+          handleAddTab={handleAddTab}
+        />
+      );
+    }
+  );
+
+  list.push(
+    <TabList
+      key={0}
+      tabId={0}
+      selectedTabId={selectedTabId}
+      tabTitle="+"
+      handleChangeTab={handleChangeTab}
+      handleAddTab={handleAddTab}
+    />
+  );
+
+  return <StyleComponent.TabContainer>{list}</StyleComponent.TabContainer>;
+}
