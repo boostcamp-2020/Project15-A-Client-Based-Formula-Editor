18f03a31e5b832feb104eff3dd6771862bdd4bd6
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-26 17:15:41 +0900

    style : toolbarContainer 스타일 추가
    1. 오른쪽에 배치할 수 있도록 수정하였습니다.

---
+++ src/components/toolbar/style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import colors from '@src/utils/colors';
+
+export const ToolbarContainer = styled.div`
+  display: flex;
+  margin-left: 35rem;
+  background-color: ${colors.mainGreen};
+  height: 59px;
+`;
