906e2ed74716d607ec3f6113c86705b29725b578
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-26 17:16:22 +0900

    refactor : app에서 fontsize와 fontcolor 제거
    1. fontsize와 fontcolor를 제거하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { addStyles, EditableMathField, StaticMathField } from 'react-mathquill';
 import Header from '@src/components/header';
-import FontDropDown from '@src/components/toolbar/FontDropDown';
-import FontColorDropdown from '@src/components/toolbar/FontColorDropdown';
+
 import { GlobalStyle } from '@src/utils/global-style';
 import Toolbar from '@src/components/toolbar';
 
@@ -14,8 +13,6 @@ const App = () => {
       <GlobalStyle />
       <Header />
       <Toolbar />
-      <FontDropDown />
-      <FontColorDropdown />
     </div>
   );
 };
