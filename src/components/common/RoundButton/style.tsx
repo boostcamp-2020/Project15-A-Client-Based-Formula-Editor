89dcae6ed1b600703200dd01cd7eb2c1f7701372
zlrlo <easy10612@gmail.com>
2020-11-29 02:38:19 +0900

    feat : 툴바에 필요한 동그란 버튼 스타일 추가

---
+++ src/components/common/RoundButton/style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import colors from '@src/utils/colors';
+
+export const RoundButton = styled.div`
+  border-radius: 3.125rem;
+  width: 2.5rem;
+  height: 2.5rem;
+  background-color: #e2e4e8;
+  cursor: pointer;
+  display: flex;
+  svg {
+    margin: auto;
+  }
+`;
