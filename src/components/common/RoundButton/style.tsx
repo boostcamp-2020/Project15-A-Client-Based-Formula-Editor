4a03d344b58ddfaa51028d8dc28e9d02e4e450c3
zlrlo <easy10612@gmail.com>
2020-11-29 22:22:28 +0900

    feat: 툴바 round button 'title' class 추가
    - 툴바 아이콘 통일성을 위해서 기존 svg 삭제 후 폰트 적용한 텍스트 삽입

--- src/components/common/RoundButton/style.tsx
+++ src/components/common/RoundButton/style.tsx
@@ -3,12 +3,23 @@ import colors from '@src/utils/colors';
 
 export const RoundButton = styled.div`
   border-radius: 3.125rem;
-  width: 2.5rem;
-  height: 2.5rem;
-  background-color: #e2e4e8;
+  width: 2rem;
+  height: 2rem;
   cursor: pointer;
   display: flex;
+  background-color: ${colors.white};
+  overflow: hidden;
+  .title {
+    margin: auto;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 1 rem;
+    font-weight: bold;
+    color: ${colors.black};
+  }
   svg {
     margin: auto;
+  }
+  &:hover {
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
   }
 `;
