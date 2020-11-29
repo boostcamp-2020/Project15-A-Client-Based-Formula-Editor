c283a581207eaf992f3a4a4a6293212b4cff8440
zlrlo <easy10612@gmail.com>
2020-11-29 22:24:34 +0900

    modify: 헤더 버튼에 colors 적용

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import SVGIcon from '@src/utils/svg/logo.svg';
 import Button from '@src/components/common/Button';
 import colors from '@src/utils/colors';
+import HeaderTitle from './HeaderTitle';
 import * as StyleComponent from './style';
 
 const Header = () => {
   return (
     <StyleComponent.HeaderContainer>
       <SVGIcon />
+      <HeaderTitle />
       <StyleComponent.ButtonContainer>
-        <Button color={colors.imageSave} value="이미지 저장" />
-        <Button color={colors.exportSave} value="Export" />
+        <Button color={colors.lightGrey} value="이미지 저장" />
+        <Button color={colors.lightGrey} value="Export" />
       </StyleComponent.ButtonContainer>
     </StyleComponent.HeaderContainer>
   );
