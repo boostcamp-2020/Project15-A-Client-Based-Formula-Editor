64d27a7dae4ceff621a295d3baefefbddc71ee37
이수경 <dltnrud987@gmail.com>
2020-11-30 12:51:51 +0900

    Refactor: 불필요 파일 삭제

--- src/components/common/title/style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import colors from '@src/utils/colors';
-
-export const TitleContainer = styled.div`
-  font-size: 20px;
-  font-weight: bold;
-  padding: 10px 10px;
-  border-bottom: 1px solid ${colors.borderGrey};
-`;
