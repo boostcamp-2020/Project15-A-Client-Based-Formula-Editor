5575ef23d910099a7692236d1e3eab6e2fb03e73
zlrlo <easy10612@gmail.com>
2020-11-30 17:50:40 +0900

    modify: 그래프 RoundButton 스타일 컴포넌트 적용
    - svg 사이즈 변경

--- src/components/Toolbar/GraphButton/style.tsx
+++ src/components/Toolbar/GraphButton/style.tsx
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
+import { RoundButton } from '@src/components/Common/RoundButton/style';
 
 interface Props {
   show: boolean;
 }
-export const ButtonContainer = styled.div`
-  background-color: transparent;
-  cursor: pointer;
+export const ButtonContainer = styled(RoundButton)`
   svg {
     background-color: transparent;
   }
