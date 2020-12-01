83610372c27c89e493d5d93925d5c358d897357f
zlrlo <easy10612@gmail.com>
2020-12-01 16:47:14 +0900

    feat: mathquill 영역 드래그앤드롭 적용
    - hover효과로 배경색 변경

--- src/components/MathQuill/index.tsx
+++ src/components/MathQuill/index.tsx
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
 import { addStyles, EditableMathField } from 'react-mathquill';
+import { useDrop } from 'react-dnd';
+import { ItemTypes } from './ItemTypes';
 import * as StyleComponent from './style';
 
 addStyles();
 
-const EditableMathExample = () => {
+const EditableMathExample: React.FC = () => {
   const [latex, setLatex] = useState('kkkk');
 
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: ItemTypes.BOX,
+    drop: () => ({ name: 'Dustbin' }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  const isActive = canDrop && isOver;
+  let backgroundColor = '#222';
+  if (isActive) {
+    backgroundColor = 'darkgreen';
+  } else if (canDrop) {
+    backgroundColor = 'darkkhaki';
+  }
+
   return (
-    <StyleComponent.MathField>
+    <StyleComponent.MathField backgroundColor={backgroundColor}>
       <EditableMathField
         latex={latex}
         onChange={(mathField) => {
--- src/components/MathQuill/style.tsx
+++ src/components/MathQuill/style.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 import colors from '@src/utils/colors';
 
-export const MathField = styled.div`
+export const MathField = styled.div<{ backgroundColor: string }>`
   width: 100%;
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: white;
+  background-color: ${(props) => props.backgroundColor};
   .mq-editable-field {
     border: none;
   }
