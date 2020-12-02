ecfbc521e232d2d347cbd8359ad9d09010d76b83
zlrlo <easy10612@gmail.com>
2020-12-02 16:47:25 +0900

    feat: latex handle 드래그 기능 추가
    - 잡아서 드래그앤드롭 가능

--- src/components/MainSection/LatexSection/Handle.tsx
+++ src/components/MainSection/LatexSection/Handle.tsx
@@ -3,9 +3,16 @@ import { useDrag } from 'react-dnd';
 import * as StyleComponent from './style';
 
 export default function Handle() {
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: 'resize' },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
     <>
-      <StyleComponent.Handle>
+      <StyleComponent.Handle ref={drag}>
         <div>^</div>
       </StyleComponent.Handle>
     </>
