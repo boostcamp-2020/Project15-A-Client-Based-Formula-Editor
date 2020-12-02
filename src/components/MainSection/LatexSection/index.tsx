1a22cebfca19ea988c2b40dba1cf1266a60e8512
zlrlo <easy10612@gmail.com>
2020-12-02 16:49:23 +0900

    refactor: 드래그앤드롭 코드 main section으로 이동

--- src/components/MainSection/LatexSection/index.tsx
+++ src/components/MainSection/LatexSection/index.tsx
@@ -1,20 +1,8 @@
-import React, { useState } from 'react';
-import { useDrop } from 'react-dnd';
+import React from 'react';
 import Handle from './Handle';
 import * as StyleComponent from './style';
 
 export default function LaTeXSection() {
-  const [height, setHeight] = useState(0);
-  const [, resizeDrop] = useDrop({
-    accept: 'resize',
-    hover(item, monitor) {
-      const dy = height + monitor.getDifferenceFromInitialOffset().y / 100;
-      setHeight(dy);
-    },
-  });
-
-  const test = 304 - height;
-
   return (
     <>
       <Handle />
