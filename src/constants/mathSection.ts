056f13f262c923e37876d755fcd671f70d9e1ff0
이수경 <dltnrud987@gmail.com>
2020-12-03 21:01:40 +0900

    feat: latex data 추가
    수식 영역에 필요한 latex data 추가

--- src/constants/mathSection.ts
+++ src/constants/mathSection.ts
@@ -1,22 +1,19 @@
-import FRACTION from '@src/utils/svg/leftsection/mathsection/fraction.svg';
-import SLASH from '@src/utils/svg/leftsection/mathsection/slash.svg';
-import SQRT from '@src/utils/svg/leftsection/mathsection/sqrt.svg';
-import COMBI from '@src/utils/svg/leftsection/mathsection/combi.svg';
+import { SVG } from '@src/utils/svg/leftsection/mathsection';
 
 export interface InputMath {
   name: string;
   svg: string;
   latex: string;
 }
-export const fraction: InputMath[] = [
+export const mathFraction: InputMath[] = [
   {
     name: 'fraction',
-    svg: FRACTION,
+    svg: SVG.fraction.FRACTION,
     latex: '\\frac{}{}',
   },
   {
     name: 'slash',
-    svg: SLASH,
+    svg: SVG.fraction.SLASH,
     latex: '/ ',
   },
 ];
@@ -24,12 +21,12 @@ export const fraction: InputMath[] = [
 export const sqrt: InputMath[] = [
   {
     name: 'sqrt1',
-    svg: SQRT,
+    svg: SVG.sqrt.SQRT,
     latex: '\\sqrt{ } ',
   },
   {
     name: 'sqrt2',
-    svg: SQRT,
+    svg: SVG.sqrt.SQRT_WITH_DEGREE,
     latex: '\\sqrt[]{} ',
   },
 ];
@@ -37,37 +34,91 @@ export const sqrt: InputMath[] = [
 export const combi: InputMath[] = [
   {
     name: 'combi1',
-    svg: COMBI,
+    svg: SVG.combi.COMBI,
     latex: 'combi{ }^{ }',
   },
   {
     name: 'combi2',
-    svg: COMBI,
+    svg: SVG.combi.COMBI_DOWN,
     latex: 'combi{ }_{ }',
   },
   {
     name: 'combi3',
-    svg: COMBI,
-    latex: 'combi{ }^{ }',
-  },
-  {
-    name: 'combi4',
-    svg: COMBI,
+    svg: SVG.combi.COMBI_RIGHT,
     latex: '\\combi{ }_{ }^{ }',
   },
   {
-    name: 'combi5',
-    svg: COMBI,
+    name: 'combi4',
+    svg: SVG.combi.COMBI_LEFT,
     latex: '\\combi{\\ }_{ }^{ }\\combi{ }',
   },
   {
-    name: 'combi6',
-    svg: COMBI,
+    name: 'combi5',
+    svg: SVG.combi.COMBI_DOWN_BOTH,
     latex: '\\combi{\\ }_{ }\\combi{ }_{ }',
   },
   {
-    name: 'combi7',
-    svg: COMBI,
+    name: 'combi6',
+    svg: SVG.combi.COMBI_ALL,
     latex: '\\combi{\\ }_{ }^{ }combi{ }_{ }^{ }',
+  },
+];
+
+export const integral: InputMath[] = [
+  {
+    name: 'integral1',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\int _{\\ }^{\\ }',
+  },
+  {
+    name: 'integral2',
+    svg: SVG.integral.DEFINITE_INTEGRAL,
+    latex: '\\int _{ }^{ }',
+  },
+  {
+    name: 'integral3',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\iint _{\\ }^{\\ }',
+  },
+  {
+    name: 'integral4',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\iint _{ }^{ }',
+  },
+  {
+    name: 'integral5',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\oint _{\\ }^{\\ }',
+  },
+  {
+    name: 'integral6',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\oint _{ }^{ }',
+  },
+  {
+    name: 'integral7',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\oiint _{\\ }^{\\ }',
+  },
+  {
+    name: 'integral8',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\oiint _{ }^{ }',
+  },
+
+  {
+    name: 'integral9',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\oiiint _{\\ }^{\\ }',
+  },
+  {
+    name: 'integral10',
+    svg: SVG.integral.INTEGRAL,
+    latex: '\\oiiint _{ }^{ }',
+  },
+  {
+    name: 'integral11',
+    svg: SVG.integral.INTEGRAL,
+    latex: ' \\left\\lceil \\right\\rceil _{ }^{ }',
   },
 ];
