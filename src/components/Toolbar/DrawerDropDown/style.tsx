ef65ee8193debba49b1a0a48063f495c8f6085ac
thdwlsgus0 <thdwlsgus0@naver.com>
2020-12-04 02:10:27 +0900

    style : 그림판 도구 css 변경
    1. border-radius을 변경하였습니다.
    2. cursor:pointer로 지정하였습니다.

--- src/components/Toolbar/DrawerDropDown/style.tsx
+++ src/components/Toolbar/DrawerDropDown/style.tsx
@@ -5,15 +5,19 @@ export const DrawerContainer = styled.div`
   justify-content: space-between;
   & svg {
     margin-top: 10px;
+    cursor: pointer;
   }
   & input {
     margin-top: 10px;
   }
 `;
 export const DrawerDiv = styled.div`
-  border-radius: 3px;
+  border-radius: 20px;
   width: 30px;
   height: 30px;
   background-color: ${(props) => props.color};
   margin-top: 10px;
+  &: hover {
+    cursor: pointer;
+  }
 `;
