7e6835d492ad3a826cc7b9a0897e9e9c8d94e8e2
이수경 <dltnrud987@gmail.com>
2020-12-03 10:37:52 +0900

    feat: 수식 입력 기능
    - button 사용하여 수식을 입력하도록 만듦

--- src/components/LeftSection/MathSection/index.tsx
+++ src/components/LeftSection/MathSection/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import Title from '@src/components/Common/Title';
+import MULTPLY from '@src/utils/svg/leftsection/inputsection/multiply.svg';
+import Svg from '@src/components/Common/Svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { insert } from '@src/store/modules/mathQuill';
+import { RootState } from '@src/store/modules';
+import { MathField } from '@src/components/MainSection/MathQuill/style';
 import * as StyledComponent from './style';
 
+const math = [
+  {
+    name: 'multiply',
+    svg: MULTPLY,
+    latex: '\\frac{ }{ }',
+  },
+];
+
 const MathSectionContainer = () => {
+  const dispatch = useDispatch();
+  const { mathQuiiFunc } = useSelector(
+    (state: RootState) => state.mathQuillReducer
+  );
+  const onClickHandler = (value: string) => {
+    mathQuiiFunc.write(value);
+    dispatch(insert(value));
+  };
   return (
     <>
       <StyledComponent.MathSectionContainer>
         <Title title="수식" />
+        {math.map((data) => (
+          <StyledComponent.Button
+            key={data.name}
+            onClick={() => onClickHandler(data.latex)}
+          >
+            <Svg Svg={data.svg} />
+          </StyledComponent.Button>
+        ))}
       </StyledComponent.MathSectionContainer>
     </>
   );
