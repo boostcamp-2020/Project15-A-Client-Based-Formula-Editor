947ae9f5b26385f9d8e78e5f2e5a1439eaf146d4
이수경 <dltnrud987@gmail.com>
2020-12-03 20:43:48 +0900

    feat: 수식 영역 svg 파일 추가

---
+++ src/utils/svg/leftsection/mathsection/combi/index.ts
@@ -0,0 +1,15 @@
+import COMBI from './combi.svg';
+import COMBI_DOWN from './combi-down.svg';
+import COMBI_DOWN_BOTH from './combi-down-both.svg';
+import COMBI_LEFT from './combi-left.svg';
+import COMBI_RIGHT from './combi-right.svg';
+import COMBI_ALL from './combi-all.svg';
+
+export default {
+  COMBI,
+  COMBI_DOWN,
+  COMBI_DOWN_BOTH,
+  COMBI_LEFT,
+  COMBI_RIGHT,
+  COMBI_ALL,
+};
