9c45c516bfb3cc85ce7afd1cad19c08e3009ad9d
zlrlo <easy10612@gmail.com>
2020-12-03 23:52:10 +0900

    feat: tab update action, reducer 추가
    - 맨 처음 localstorage에서 데이터 가져와서 tabList update하는 기능

--- src/store/modules/tab.ts
+++ src/store/modules/tab.ts
@@ -2,22 +2,27 @@ import { createAction, handleActions } from 'redux-actions';
 
 const CHANGE_TAB = 'tab/CHANGE_TAB' as const;
 const ADD_TAB = 'tab/ADD_TAB' as const;
+const UPDATE_TAB = 'tab/UPDATE_TAB' as const;
 
 export const changeTab = createAction(CHANGE_TAB);
 export const addTab = createAction(ADD_TAB);
+export const updateTab = createAction(UPDATE_TAB);
 
-type Action = ReturnType<typeof changeTab> | ReturnType<typeof addTab>;
+type Action =
+  | ReturnType<typeof changeTab>
+  | ReturnType<typeof addTab>
+  | ReturnType<typeof updateTab>;
 
 export interface TabState {
   lastId: number;
   selectedTabId: number;
-  tabList: { id: number; title: string }[];
+  tabList: { id: number; title: string; latex: string }[];
 }
 
 const initialState = {
   lastId: 1,
   selectedTabId: 1,
-  tabList: [{ id: 1, title: 'TAB1' }],
+  tabList: [{ id: 1, title: 'TAB1', latex: 'blank' }],
 };
 
 export const tabReducer = handleActions(
@@ -25,11 +30,22 @@ export const tabReducer = handleActions(
     [CHANGE_TAB]: (state, action: Action) => {
       return { ...state, selectedTabId: action.payload };
     },
-    [ADD_TAB]: (state, action: Action) => {
+    [ADD_TAB]: (state) => {
       return {
         ...state,
         lastId: state.lastId + 1,
-        tabList: state.tabList.concat({ id: state.lastId + 1, title: 'TAB1' }),
+        tabList: state.tabList.concat({
+          id: state.lastId + 1,
+          title: `TAB${state.lastId + 1}`,
+          latex: 'blank',
+        }),
+      };
+    },
+    [UPDATE_TAB]: (state, action: Action) => {
+      return {
+        ...state,
+        lastId: action.payload.length,
+        tabList: action.payload,
       };
     },
   },
