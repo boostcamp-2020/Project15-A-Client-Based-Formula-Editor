999d16b364ce4480ef2d601790935d42023de9c2
thdwlsgus0 <thdwlsgus0@naver.com>
2020-12-05 15:38:39 +0900

    feature : crop을 위한 상태변화 구현
    1. crop을 위한 상태변화를 구현하였습니다.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SVGIcon from '@src/utils/svg/header/logo.svg';
 import Button from '@src/components/Common/Button';
 import colors from '@src/utils/colors';
@@ -6,19 +6,23 @@ import html2canvas from 'html2canvas';
 import { RootState } from '@src/store/modules';
 import { useSelector, useDispatch } from 'react-redux';
 import saveAsFile from '@src/utils/savefile';
+import { setCropContainer } from '@src/store/modules/getMathQuill';
 import HeaderTitle from './HeaderTitle';
 import * as StyleComponent from './style';
 
 const Header = () => {
-  const { mathQuillContainer } = useSelector(
+  const { mathQuillContainer, click } = useSelector(
     (state: RootState) => state.getMathQuillReducer
   );
   const { mathQuiiFunc } = useSelector(
     (state: RootState) => state.mathQuillReducer
   );
+  const dispatch = useDispatch();
   const onClickImageSaveHandler = async () => {
     const mathquillSection = mathQuillContainer.current;
     const canvas = await html2canvas(mathquillSection);
+    dispatch(setCropContainer(true));
+    console.log(click);
     const imageUrl = canvas.toDataURL('image/gif');
     const aTag = document.createElement('a');
     document.body.appendChild(aTag);
