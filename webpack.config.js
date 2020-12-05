95b4d480d36e6d1109efdaec40d5c884630f6a4a
thdwlsgus0 <thdwlsgus0@naver.com>
2020-12-06 01:03:11 +0900

    build : favicon 설정
    1. 수식판의 로고에 맞는 favicon을 설정하였습니다.

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './public/index.html', // public/index.html 파일을 읽는다.
       filename: 'index.html', // output으로 출력할 파일은 index.html 이다.
+      favicon: 'public/favicon.ico',
     }),
     new CleanWebpackPlugin({
       cleanAfterEveryBuildPatterns: ['build'],
