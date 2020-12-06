4e86dafd824cf71a97755430f1b191bb3f6c7e91
zlrlo <easy10612@gmail.com>
2020-12-06 20:06:28 +0900

    refactor: 드롭다운 버튼 핸들러 코드 이동

--- src/components/Toolbar/BackgroundDropdown/index.tsx
+++ src/components/Toolbar/BackgroundDropdown/index.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@src/store/modules';
 import SPRING from '@src/utils/svg/background/spring.svg';
 import SUMMER from '@src/utils/svg/background/summer.svg';
 import FALL from '@src/utils/svg/background/fall.svg';
 import WINTER from '@src/utils/svg/background/winter.svg';
 import BLACKBOARD from '@src/utils/svg/background/blackboard.svg';
+import BACKGROUND from '@src/utils/svg/background/background_icon.svg';
+import { RoundButton } from '@src/components/Common/RoundButton/style';
+import { backgroundDropdown } from '@src/store/modules/backgroundDropdown';
 import * as StyleComponent from './style';
 
 const Background = () => {
+  const { isBackgroundDropdownShow } = useSelector(
+    (state: RootState) => state.BackgroundDropdownHandler
+  );
+
+  const dispatch = useDispatch();
+
+  const onClickBackgroundHandler = () => {
+    dispatch(backgroundDropdown(!isBackgroundDropdownShow));
+  };
+
   return (
-    <StyleComponent.BackgroundContainer>
-      <SPRING />
-      <SUMMER />
-      <FALL />
-      <WINTER />
-      <BLACKBOARD />
-    </StyleComponent.BackgroundContainer>
+    <div>
+      <RoundButton onClick={onClickBackgroundHandler}>
+        <BACKGROUND />
+      </RoundButton>
+      {isBackgroundDropdownShow && (
+        <StyleComponent.BackgroundContainer>
+          <SPRING />
+          <SUMMER />
+          <FALL />
+          <WINTER />
+          <BLACKBOARD />
+        </StyleComponent.BackgroundContainer>
+      )}
+    </div>
   );
 };
 
