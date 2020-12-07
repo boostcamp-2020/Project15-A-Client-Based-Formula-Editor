d296d5ce452e58b683625162295353db2b573bae
zlrlo <easy10612@gmail.com>
2020-12-07 20:51:22 +0900

    modify: title 스타일 변경
    - padding 줄임
    - box-shadow 추가

--- src/components/Common/Title/style.tsx
+++ src/components/Common/Title/style.tsx
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 import colors from '@src/utils/colors';
 
 export const TitleContainer = styled.div`
-  font-size: 20px;
+  font-size: 17px;
   font-weight: bold;
-  padding: 10px 10px;
+  padding: 7px 7px;
   border-bottom: 1px solid ${colors.borderGrey};
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.1);
 `;
