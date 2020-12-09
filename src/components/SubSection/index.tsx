83de79cc76e0db012130023724aa8f996ee2974f
zlrlo <easy10612@gmail.com>
2020-12-09 22:06:24 +0900

    feat: 그래프 타이틀 생성중

--- src/components/SubSection/index.tsx
+++ src/components/SubSection/index.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Desmos from 'desmos';
 import * as StyleComponent from './style';
 
@@ -10,19 +10,26 @@ interface Props {
 
 const SubSection = ({ visible }: Props) => {
   const canvasRef = useRef<HTMLDivElement>(null);
-  console.log(canvasRef);
-  const calculator = Desmos.GraphingCalculator(canvasRef.current);
+  const calculator = Desmos.GraphingCalculator(canvasRef.current, {
+    expressions: false,
+    settingsMenu: false,
+    zoomButtons: false,
+    trace: false,
+    border: false,
+    lockViewport: true,
+  });
   calculator.setExpression({ id: 'graph1', latex: 'y=x^2' });
 
-  // const html = elt.innerHTML;
-
-  // const createMarkup = () => {
-  //   console.log(html);
-  //   return { __html: `${html}` };
-  // };
+  const onClickHandler = () => {
+    calculator.destroy();
+  };
 
   return (
     <StyleComponent.RightArea show={visible}>
+      <StyleComponent.Title onClick={onClickHandler}>
+        <div className="edge" />
+        <div className="title">Graph</div>
+      </StyleComponent.Title>
       <StyleComponent.GraphContainer ref={canvasRef} />
     </StyleComponent.RightArea>
   );
--- src/components/SubSection/style.ts
+++ src/components/SubSection/style.ts
@@ -11,10 +11,31 @@ export const RightArea = styled.div<Props>`
   flex-grow: ${(props) => (props.show ? '1' : '0')};
   overflow: hidden;
   background-color: ${colors.backgroundGrey};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const GraphContainer = styled.div`
-  border: 3px solid;
+  width: 400px;
+  height: 300px;
+  border: 1px solid;
+`;
+
+export const Title = styled.div`
   width: 100%;
-  height: 500px;
+  height: 45px;
+  border: 1px solid;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  display: flex;
+  align-items: center;
+  .edge {
+    width: 10px;
+    height: 100%;
+    background-color: ${colors.mainGreen};
+  }
+  .title {
+    font-size: 30px;
+  }
 `;
