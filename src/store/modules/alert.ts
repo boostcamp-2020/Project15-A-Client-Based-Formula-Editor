6408e32339846e65fb69429a16ee00d15715985c
zlrlo <easy10612@gmail.com>
2020-12-16 15:29:34 +0900

    feat: 알람창 관련 reducer 코드 작성
    - mode : 0, 1, 2 가능
    - 0: info, 1: error, 2: success
    - toggle : 클릭 마다 0, 1 토글

---
+++ src/store/modules/alert.ts
@@ -0,0 +1,29 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const CHANGE_ALERT_MODE = 'alert/CHANGE_ALERT_MODE' as const;
+
+export const changeAlertMode = createAction(CHANGE_ALERT_MODE);
+
+type Action = ReturnType<typeof changeAlertMode>;
+
+export interface AlertModeState {
+  mode: number;
+  toggle: boolean;
+}
+
+const initialState = {
+  mode: 0,
+  toggle: false,
+};
+
+export const alertReducer = handleActions(
+  {
+    [CHANGE_ALERT_MODE]: (state, action: Action) => {
+      if (state.toggle)
+        return { ...state, mode: action.payload, toggle: false };
+      if (!state.toggle)
+        return { ...state, mode: action.payload, toggle: true };
+    },
+  },
+  initialState
+);
