e459e5a0f82df3fecf735ef68f083b6813eabff5
zlrlo <easy10612@gmail.com>
2020-12-16 15:25:13 +0900

    feat: alert창 분기 처리
    - 0: info 알림용
    - 1: error 알림용
    - 2: success 알림용

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '@src/store/modules';
 import * as StyleComponent from './style';
 
 const Alert = () => {
+  const { mode, toggle } = useSelector(
+    (state: RootState) => state.alertReducer
+  );
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (mode !== 0) {
+      setVisible(true);
+
+      setTimeout(() => {
+        setVisible(false);
+      }, 2500);
+    }
+  }, [toggle]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(true);
+    }, 1000);
+
+    setTimeout(() => {
+      setVisible(false);
+    }, 7000);
+  }, []);
+
   return (
-    <StyleComponent.MiddleHolder>
-      <div className="alert alert-error">
-        <strong>Oh snap! </strong>
-        Change a few things up and try submitting again.
-      </div>
-      <div className="alert alert-success">
-        <strong>Well done! </strong>
-        You successfully read this important alert message.
-      </div>
-      <div className="alert alert-info">
-        <strong>Heads up! </strong>
-        This alert needs your attention, but its not super important.
-      </div>
-    </StyleComponent.MiddleHolder>
+    <>
+      <StyleComponent.MiddleHolder visible={visible}>
+        {(function () {
+          if (mode === 0) {
+            return (
+              <div className="alert alert-info">
+                <strong>Info💡 </strong>
+                Try drag and drop here!
+              </div>
+            );
+          }
+          if (mode === 1) {
+            return (
+              <div className="alert alert-error">
+                <strong>sorry🙏 </strong>
+                This feature is still being prepared.
+              </div>
+            );
+          }
+          if (mode === 2) {
+            return (
+              <div className="alert alert-success">
+                <strong>Well done!👐 </strong>
+                You have successfully saved.
+              </div>
+            );
+          }
+          return undefined;
+        })()}
+      </StyleComponent.MiddleHolder>
+    </>
   );
 };
 
