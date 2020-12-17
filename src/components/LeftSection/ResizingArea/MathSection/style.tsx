a6e5ec5426e3f6d5dd57c4c4cdc12844a6ed9394
zlrlo <easy10612@gmail.com>
2020-12-18 04:09:16 +0900

    modify: 스크롤 스타일 변경
    - 왼쪽 영역 적용
    - 탭 영역 적용

--- src/components/LeftSection/ResizingArea/MathSection/style.tsx
+++ src/components/LeftSection/ResizingArea/MathSection/style.tsx
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import colors from '@src/utils/colors';
 
 export const MathSectionContainer = styled.div`
-  width: 97%;
-  height: 100%;
   display: flex;
   flex-direction: column;
+  width: 97%;
+  height: 100%;
   border: 1px solid ${colors.borderGrey};
   background-color: ${colors.white};
 `;
@@ -13,14 +13,30 @@ export const ButtonContainer = styled.div`
   flex: 1;
   display: flex;
   flex-wrap: wrap;
-  overflow: auto;
   align-content: flex-start;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background: ${colors.white};
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 3.5px;
+    background-color: ${colors.lightGrey};
+
+    &:hover {
+      background-color: ${colors.grey};
+    }
+  }
+  &::-webkit-scrollbar-track {
+    background: ${colors.white};
+  }
 `;
 
 export const LoadingContainer = styled.div`
+  display: flex;
   width: 100%;
   height: 100%;
-  display: flex;
   align-items: center;
   justify-content: center;
 `;
--- src/components/LeftSection/ResizingArea/TemplateSection/style.tsx
+++ src/components/LeftSection/ResizingArea/TemplateSection/style.tsx
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 import colors from '@src/utils/colors';
 
 export const TemplateSectionContainer = styled.div`
-  width: 97%;
-  height: 100%;
   display: flex;
   flex-direction: column;
-  background-color: ${colors.white};
+  width: 97%;
+  height: 100%;
   border: 1px solid ${colors.borderGrey};
+  background-color: ${colors.white};
 `;
 
 export const Header = styled.div`
@@ -18,9 +18,9 @@ export const Header = styled.div`
     height: 22px;
     margin-left: auto;
     margin-right: 10px;
+    outline: none;
     border: none;
     background-color: ${colors.white};
-    outline: none;
     opacity: 0.6;
     cursor: pointer;
   }
@@ -39,4 +39,20 @@ export const ButtonContainer = styled.div`
   flex-wrap: wrap;
   align-content: flex-start;
   overflow: auto;
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background: ${colors.white};
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 3.5px;
+    background-color: ${colors.lightGrey};
+
+    &:hover {
+      background-color: ${colors.grey};
+    }
+  }
+  &::-webkit-scrollbar-track {
+    background: ${colors.white};
+  }
 `;
