3ed44cecddd9c499349d33e6a52086cffe8b83b8
zlrlo <easy10612@gmail.com>
2020-12-18 01:05:41 +0900

    refactor: splitArea안으로 수식영역 템플릿 영역 이동

---
+++ src/components/LeftSection/SplitArea/TemplateSection/style.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import colors from '@src/utils/colors';
+
+export const TemplateSectionContainer = styled.div`
+  width: 97%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.white};
+  border: 1px solid ${colors.borderGrey};
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  button {
+    width: 28px;
+    height: 22px;
+    margin-left: auto;
+    margin-right: 10px;
+    border: none;
+    background-color: ${colors.white};
+    outline: none;
+    opacity: 0.6;
+    cursor: pointer;
+  }
+
+  button:hover {
+    opacity: 1;
+  }
+
+  & > button > svg {
+    fill: ${colors.grey};
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  overflow: auto;
+`;
