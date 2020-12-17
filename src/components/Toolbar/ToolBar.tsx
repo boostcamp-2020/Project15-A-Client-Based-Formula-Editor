d0e173aa96edf17e8a9f58099920e42902889306
zlrlo <easy10612@gmail.com>
2020-12-18 01:09:00 +0900

    refactor: 디렉토리명 변경
    - fontSizeDropdown
    - HistoryButtons

--- src/components/Toolbar/ToolBar.tsx
+++ src/components/Toolbar/ToolBar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import FontSizeDropDown from '@src/components/Toolbar/FontDropDown';
+import FontSizeDropDown from '@src/components/Toolbar/FontSizeDropdown';
 import FontColorDropDown from '@src/components/Toolbar/FontColorDropdown';
 import ItalicButton from '@src/components/Toolbar/ItalicButton';
 import DrawerDropDown from '@src/components/Toolbar/PaintDropDown';
 import BackgroundDropDown from '@src/components/Toolbar/BackgroundDropdown';
 import AlignDropDown from '@src/components/Toolbar/AlignDropDown';
 import GraphButton from '@src/components/Toolbar/GraphButton';
-import OptionButtons from '@src/components/Toolbar/OptionButtons';
+import OptionButtons from '@src/components/Toolbar/HistoryButtons';
 import ToolBarTemplate from './ToolBarTemplate';
 
 const ToolBar = () => {
