5fa0c43d1670417ccad58c7d87258ae0f9c23e71
zlrlo <easy10612@gmail.com>
2020-12-18 04:04:10 +0900

    chore: 스타일컴포넌트 속성 순서 정리

--- src/components/Common/Title/style.tsx
+++ src/components/Common/Title/style.tsx
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
-import colors from '@src/utils/colors';
 
 export const TitleContainer = styled.div`
   flex: none;
   display: flex;
   height: 33px;
   padding-left: 7px;
-  align-items: center;
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.1);
+  align-items: center;
   font-size: 17px;
   font-weight: bold;
 `;
