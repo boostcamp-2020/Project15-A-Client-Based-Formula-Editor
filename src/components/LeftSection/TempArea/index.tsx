6b81207ff98dad6dd8974cfbbf10a15f9d54e305
zlrlo <easy10612@gmail.com>
2020-12-18 01:06:28 +0900

    refactor: tempArea 명칭 변경
    - 여백영역으로 명칭 변경

--- src/components/LeftSection/TempArea/index.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import React from 'react';
-import * as StyledComponent from './style';
-
-const TempArea = () => {
-  return <StyledComponent.Temp />;
-};
-
-export default TempArea;
--- src/components/LeftSection/TempArea/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import colors from '@src/utils/colors';
-
-export const Temp = styled.div`
-  width: 97%;
-  height: 3%;
-  background-color: ${colors.borderGrey};
-`;
