6c0395268de4feb77b835bc338820e78dafe42d4
zlrlo <easy10612@gmail.com>
2020-12-17 17:31:25 +0900

    refactor: backgroundDropdown action명 수정
    - 대문자, 소문자 통일

--- src/store/modules/backgroundDropdown.ts
+++ src/store/modules/backgroundDropdown.ts
@@ -1,16 +1,16 @@
 import { createAction, handleActions } from 'redux-actions';
 
-const CHANGE_DROPDOWN = 'backgroundDropdown/CHANGE_DROPDOWN' as const;
+const SHOW_BACKGROUND_DROPDOWN = 'backgroundDropdown/SHOW_BACKGROUND_DROPDOWN' as const;
 const GET_CANVAS = 'backgroundDropdown/GET_CANVAS' as const;
-const WINTER_DROPDOWN = 'backgroundDropdown/WINTER_DROPDOWN' as const;
-const SUMMER_DROPDOWN = 'backgroundDropdown/SUMMER_DROPDOWN' as const;
-const CLOSE_DROPDOWN = 'backgroundDropdown/CLOSE_DROPDOWN' as const;
+const SHOW_WINTER = 'backgroundDropdown/SHOW_WINTER' as const;
+const SHOW_SUMMER = 'backgroundDropdown/SHOW_SUMMER' as const;
+const CLOSE_BACKGROUND_DROPDOWN = 'backgroundDropdown/CLOSE_BACKGROUND_DROPDOWN' as const;
 
-export const showBackgroundDropdown = createAction(CHANGE_DROPDOWN);
+export const showBackgroundDropdown = createAction(SHOW_BACKGROUND_DROPDOWN);
 export const getCanvas = createAction(GET_CANVAS);
-export const showWinter = createAction(WINTER_DROPDOWN);
-export const showSummer = createAction(SUMMER_DROPDOWN);
-export const closeBackgroundDropdown = createAction(CLOSE_DROPDOWN);
+export const showWinter = createAction(SHOW_WINTER);
+export const showSummer = createAction(SHOW_SUMMER);
+export const closeBackgroundDropdown = createAction(CLOSE_BACKGROUND_DROPDOWN);
 
 type Action =
   | ReturnType<typeof showBackgroundDropdown>
@@ -25,6 +25,7 @@ export interface BackgroundState {
   winterBackground: boolean;
   summerBackground: boolean;
 }
+
 const initialState: BackgroundState = {
   backgroundDropdown: false,
   backgroundCanvas: '',
@@ -32,9 +33,9 @@ const initialState: BackgroundState = {
   summerBackground: false,
 };
 
-export const BackgroundDropdownHandler = handleActions(
+export const backgroundDropdownReducer = handleActions(
   {
-    [CHANGE_DROPDOWN]: (state: BackgroundState = initialState) => {
+    [SHOW_BACKGROUND_DROPDOWN]: (state: BackgroundState = initialState) => {
       return {
         ...state,
         backgroundDropdown: !state.backgroundDropdown,
@@ -46,19 +47,19 @@ export const BackgroundDropdownHandler = handleActions(
         backgroundCanvas: action.payload,
       };
     },
-    [WINTER_DROPDOWN]: (state: BackgroundState = initialState) => {
+    [SHOW_WINTER]: (state: BackgroundState = initialState) => {
       return {
         ...state,
         winterBackground: !state.winterBackground,
       };
     },
-    [SUMMER_DROPDOWN]: (state: BackgroundState = initialState) => {
+    [SHOW_SUMMER]: (state: BackgroundState = initialState) => {
       return {
         ...state,
         summerBackground: !state.summerBackground,
       };
     },
-    [CLOSE_DROPDOWN]: (state: BackgroundState = initialState) => {
+    [CLOSE_BACKGROUND_DROPDOWN]: (state: BackgroundState = initialState) => {
       return { ...state, backgroundDropdown: false };
     },
   },
