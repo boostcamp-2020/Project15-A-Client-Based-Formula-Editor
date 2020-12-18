401705581f14fe5e5b269d024c31f1cd0e2d295e
이수경 <dltnrud987@gmail.com>
2020-12-18 11:37:55 +0900

    style : 템플릿 영역 style 수정
    - 제목 아랫부분 밑줄 100%로 수정

--- src/components/Common/Title/style.tsx
+++ src/components/Common/Title/style.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const TitleContainer = styled.div`
   flex: none;
   display: flex;
+  width: 100%;
   height: 33px;
   padding-left: 7px;
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.1);
