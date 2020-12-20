5b2f1b6d0411dcb72a33b1fe6c9a879590338f14
이수경 <dltnrud987@gmail.com>
2020-12-20 22:46:26 +0900

    style: left Section width 조절

--- src/components/LeftSection/BottomWhiteSpace/style.ts
+++ src/components/LeftSection/BottomWhiteSpace/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import colors from '@src/utils/colors';
 
 export const BottomWhiteSpace = styled.div`
-  width: 97%;
+  width: 98%;
   height: 3%;
   background-color: ${colors.borderGrey};
 `;
