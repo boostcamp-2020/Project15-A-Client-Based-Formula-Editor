a7656adf28cfda4552cdb821b4cb2e0a058d0c56
이수경 <dltnrud987@gmail.com>
2020-12-20 14:17:49 +0900

    refactor: 초기 수식영역 제거
    - click이 안되는 문제 때문에 제거

--- src/components/LeftSection/ResizingArea/MathSection/index.tsx
+++ src/components/LeftSection/ResizingArea/MathSection/index.tsx
@@ -8,7 +8,6 @@ import { LatexContent } from '@src/components/Common/LatexContent';
 import { DictionaryContent } from '@src/components/Common/DictionaryContent';
 import { LatexSymbolContent } from '@src/components/Common/LatexSymbolContent';
 import mathSection from '@src/constants/mathSection';
-import * as mathSVG from '@src/constants/mathSection';
 import { changeAlertMode } from '@src/store/modules/alert';
 import * as StyledComponent from './style';
 import LatexContentList from './ButtonList';
@@ -27,7 +26,7 @@ const MathSectionContainer = () => {
   );
 
   const math = mathSection.filter((id) => id.name === name);
-  const mathArray = math.length > 0 ? math[0].value : mathSVG.mathFraction;
+  const mathArray = math.length > 0 ? math[0].value : [];
 
   const onClickHandler = (value: string, isPossible: boolean) => {
     if (isPossible) {
