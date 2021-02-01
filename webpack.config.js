3b65874f7d21246a81acfde0ddfc649c25da2ad3
thdwlsgus0 <thdwlsgus0@naver.com>
2021-02-01 17:12:45 +0900

    chore: polyfill 미적용 수정

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index',
+  entry: ['@babel/polyfill', './src/index'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(`${__dirname}/build`),
